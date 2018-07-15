8d1ff83b02d6956cec17c6785809fcabd2117019
HallowXIII <zephyrnox@gmail.com>
2018-07-15 18:57:42 +0200

    Allow multiple connections, deal with raw and cooked mode.

--- lib/ymprotocol.ex
+++ lib/ymprotocol.ex
@@ -18,24 +18,39 @@ defmodule Ymserver.YmProtocol do
     :ok = :ranch.accept_ack(ref)
     :ok = transport.setopts(socket, [{:active, true}])
     {:ok, eserv} = GenServer.start_link(Ymserver.EventServer,
-      Ymserver.TestGame.begin(), name: YmEvServ)
-    :gen_server.enter_loop(__MODULE__, [], %{socket: socket, transport: transport, eserv: eserv})
+      Ymserver.TestGame.begin())
+    :gen_server.enter_loop(__MODULE__, [], %{socket: socket, transport: transport, eserv: eserv, buf: ""})
   end
 
-  def handle_info({:tcp, socket, data}, state = %{socket: socket, transport: transport, eserv: eserv}) do
-    IO.inspect data
-    rmess = case String.trim(data) do
-              "describe" -> GenServer.call(eserv, :describe)
-              "exits" -> GenServer.call(eserv, :exits)
-              "north" ->  GenServer.call(eserv, :north)
-              "east" ->   GenServer.call(eserv, :east)
-              "west" -> GenServer.call(eserv, :west)
-              "south" ->  GenServer.call(eserv, :south)
-              "quit" -> "Quitting"
-              _ -> "Unknown command"
-            end
-    transport.send(socket, rmess <> "\r\n")
-    {:noreply, state}
+  def handle_event(state = %{socket: socket, transport: transport,
+    eserv: eserv, buf: buf}) do
+      IO.inspect buf
+      rmess = case buf do
+                "describe" -> GenServer.call(eserv, :describe)
+                "exits" -> GenServer.call(eserv, :exits)
+                "north" ->  GenServer.call(eserv, :north)
+                "east" ->   GenServer.call(eserv, :east)
+                "west" -> GenServer.call(eserv, :west)
+                "south" ->  GenServer.call(eserv, :south)
+                "quit" -> "Quitting"
+                _ -> "Unknown command"
+              end
+      transport.send(socket, rmess <> "\r\n")
+      {:noreply, Map.put(state, :buf, "")}
+  end
+
+  @doc """
+  Handles tcp data. Allows for clients in both raw and cooked mode.
+  """
+  def handle_info({:tcp, socket, data}, state = %{socket: socket, buf: buf}) do
+    cond do
+      String.match?(data, ~r/.*\r\n/) ->
+        handle_event(Map.put(state, :buf, buf <> String.trim(data)))
+      data == "\b" ->
+        {:noreply, Map.put(state, :buf, String.slice(buf, 0..-2))}
+      true ->
+        {:noreply, Map.put(state, :buf, buf <> data)}
+    end
   end
 
   def handle_info({:tcp_closed, socket}, state = %{socket: socket, transport: transport}) do
