8261bc5f773505ea6a8688b1f68f3fc741b055ad
HallowXIII <zephyrnox@gmail.com>
2018-07-30 23:55:05 +0200

    get rid of singleton

--- lib/ymserver.ex
+++ lib/ymserver.ex
@@ -7,7 +7,7 @@ defmodule Ymserver do
   @doc """
   """
   def start(_type, _args) do
-    Singleton.start_child(Ymserver.GlobalState, [1], :global_state)
+    GenServer.start_link(Ymserver.GlobalState, [], name: {:global, :global_state})
     :ranch.start_listener(:ym_listener, 100, :ranch_tcp,
       [port: 5555], Ymserver.YmProtocol, [])
   end
